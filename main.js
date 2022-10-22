82c7540b86c0bfb9c85f018d02c8556b0b29829b
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-10-22 21:59:20 +0500

    Changed random number limit and translated texts

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const attempts = document.querySelector(".js-attampts");
 const answer = document.querySelector(".js-answer");
 const finishAnswer = document.querySelector(".js-random-number");
 
-const randomNum = Math.round(Math.random() * 100);
+const randomNum = Math.round(Math.random() * 10);
 console.log(randomNum);
 
-let maxAttemptsCount = 6;
+let maxAttemptsCount = 5;
 attempts.textContent = `Attempts count: ${maxAttemptsCount}`;
 
 elForm.addEventListener("submit", function (evt) {
@@ -20,15 +20,15 @@ elForm.addEventListener("submit", function (evt) {
    attempts.textContent = `Attempts count: ${maxAttemptsCount}`;
 
    if (elInputValue < randomNum) {
-      answer.textContent = "Kiritilgan raqam random sonidan kichik";
+      answer.textContent = "The number entered is smaller than the random number";
       answer.classList.add("alert-warning");
       answer.classList.remove("alert-danger");
    } else if (elInputValue > randomNum) {
-      answer.textContent = "Kiritilgan raqam random sonidan katta";
+      answer.textContent = "The number entered is greater than the random number";
       answer.classList.remove("alert-warning");
       answer.classList.add("alert-danger");
    } else {
-      answer.textContent = "Random sondi topdingiz";
+      answer.textContent = "You have found the random number ✔";
       answer.classList.remove("alert-warning");
       answer.classList.remove("alert-danger");
       answer.classList.add("alert-success");
@@ -37,12 +37,13 @@ elForm.addEventListener("submit", function (evt) {
    if (maxAttemptsCount === 0) {
       elInput.disabled = true;
       elBtn.setAttribute("disabled", true);
-      attempts.textContent = "Urunishlar soni: 0";
+      attempts.textContent = "Attempts count: 0";
       answer.textContent = "Game over !"
       answer.classList.add("alert-dark");
       answer.classList.add("fw-bold");
       answer.classList.add("text-uppercase");
       finishAnswer.classList.add("alert-primary")
+      finishAnswer.classList.add("fw-bold")
       finishAnswer.textContent = `Random number was -> ${randomNum}`;
    }
 
